db0c988bb807a5dc73897355421294a8489ae178
VINAYSAI31 <vinaysai1133@gmail.com>
2025-01-18 11:45:40 +0530

    updated java compiler

--- src/main/java/com/vinay/controller/AdminController.java
+++ src/main/java/com/vinay/controller/AdminController.java
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
-@CrossOrigin(origins = "http://localhost:3001", allowCredentials = "true")
+@CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
 @RestController
 @RequestMapping("/api/admin")
 public class AdminController {
